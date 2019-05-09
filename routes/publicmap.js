d21a1eb0223c52a9c9d8394c3627e428ddcaf935
Adrien Chapelet <adrien3d@gmail.com>
2019-05-09 16:02:17 +0200

    Adding Monarch layer

--- routes/publicmap.js
+++ routes/publicmap.js
@@ -5,44 +5,58 @@ const http = config.https ? require('https') : require('http');
 const fs = require('fs');
 const _ = require('lodash');
 
-
-const tokenRetrieval = {
+const tokenPublicRetrieval = {
     hostname: config.sigfoxApiSite,
     path: config.publicMapPath,
     auth: config.sigfoxApiUser + ":" + config.sigfoxApiPassword
 };
 
+const tokenMonarchRetrieval = {
+    hostname: config.sigfoxApiSite,
+    path: config.monarchMapPath,
+    auth: config.sigfoxApiUser + ":" + config.sigfoxApiPassword
+};
+
 const filtered = ["AND", "ARE", "ARG", "AUS", "AUT", "BEL", "BRA", "CAN", "CHE", "CHL", "COL", "CRI", "CZE", "DEU", "DNK", "ECU", "ESP", "EST", "FIN", "FRA", "GBR", "GTM", "HKG", "HND", "HRV", "HUN", "IRL", "IRN", "ITA", "JPN", "KEN", "KOR", "LIE", "LUX", "MEX", "MLT", "MUS", "MYS", "NCL", "NLD", "NOR", "NZL", "OMN", "PAN", "PER", "PRI", "PRT", "PYF", "ROU", "SGP", "SLV", "SVK", "SWE", "THA", "TUN", "TWN", "URY", "USA", "ZAF"];
+//const filtered = [];
 const geoJsonParsed = JSON.parse(fs.readFileSync('assets/WorldCountries.geojson'));
 const countriesToDisplay = _.filter(geoJsonParsed.features, feature => _.find(filtered, e => e === feature.properties.ISO_A3) !== undefined);
 
 
-
 router.get('/', function(req, res, next) {
-    // Get the map address
-    http.get(tokenRetrieval, (sigfoxResponse) => {
-        let buff = "";
-        sigfoxResponse.on('data', d => buff += d);
-        sigfoxResponse.on('end', () => {
-            if (sigfoxResponse.statusCode !== 200) {
-            } else {
-                const mapUrl = JSON.parse(buff).tmsTemplateUrl ;
-                res.render('publicmap', {
-                    title: 'Sigfox public map',
-                    sigfoxMapUrl: mapUrl,
-                    backgroundMap: config.backgroundMap,
-                    countries: JSON.stringify(countriesToDisplay)
+    // Get the public map
+    http.get(tokenPublicRetrieval, (sigfoxResponse) => {
+    let buff = "";
+    sigfoxResponse.on('data', d => buff += d);
+    sigfoxResponse.on('end', () => {
+        if (sigfoxResponse.statusCode !== 200) {
+        } else {
+            const publicMapUrl = JSON.parse(buff).tmsTemplateUrl ;
+            // Get the monarch map
+            http.get(tokenMonarchRetrieval, (sigfoxResponse) => {
+                let buff = "";
+                sigfoxResponse.on('data', d => buff += d);
+                sigfoxResponse.on('end', () => {
+                    if (sigfoxResponse.statusCode !== 200) {
+                    } else {
+                        const monarchMapUrl = JSON.parse(buff).tmsTemplateUrl ;
+                        res.render('publicmap', {
+                            title: 'Sigfox public map',
+                            sigfoxMapUrl: publicMapUrl,
+                            sigfoxMonarchMapUrl: monarchMapUrl,
+                            backgroundMap: config.backgroundMap,
+                            countries: JSON.stringify(countriesToDisplay)
+                        });
+                    }
                 });
-            }
-        })
-
+            });
+        }
+    })
     });
 });
 
 router.get('/countries', (req, res) => {
-
     res.send(countriesToDisplay);
-
 });
 
 module.exports = router;
